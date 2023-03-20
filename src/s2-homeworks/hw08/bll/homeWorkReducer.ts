2ea310e07d3710cddc0cdcd983f9411e0ac4ed57
SoundShocking <nireemk@gmail.com>
2023-03-20 20:55:58 +0200

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,24 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: 'sort'; payload: 'up' | 'down' }
+	| { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+	switch (action.type) {
+		case 'sort': {
+			return [...state].sort((a, b) => {
+				if (action.payload === 'up') {
+					return a.name.localeCompare(b.name)
+				}
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
+				return b.name.localeCompare(a.name)
+			})
+		}
+		case 'check': {
+			return state.filter(u => u.age >= action.payload)
+		}
+		default:
+			return state
+	}
 }
